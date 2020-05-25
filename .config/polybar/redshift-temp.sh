094181c967d7a64ae6963bdee71d589266b8de3c
Hilmar Wiegand <me@hwgnd.de>
2020-05-25 22:28:21 +0200

    Add redshift module to polybar

---
+++ .config/polybar/redshift-temp.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+ICON=
+if [ "$(pgrep -x redshift)" ]; then
+    temp=$(redshift -p 2> /dev/null | grep temp | cut -d ":" -f 2 | tr -dc "[:digit:]")
+    short=$(echo "$temp / 1000" | bc -l)
+    withk=$(printf "%.1fk" $short)
+
+    if [ -z "$temp" ]; then
+        echo "$withk  %{T6}%{F#65737E} $ICON %{F-}"
+    elif [ "$temp" -ge 5000 ]; then
+        echo "$withk  %{T6}%{F#8FA1B3} $ICON %{F-}"
+    elif [ "$temp" -ge 4000 ]; then
+        echo "$withk  %{T6}%{F#EBCB8B} $ICON %{F-}"
+    else
+        echo "$withk  %{T6}%{F#D08770} $ICON %{F-}"
+    fi
+fi
