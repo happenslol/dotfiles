982602669be471017ec9fbf38d6a741c0552646d
Hilmar Wiegand <me@hwgnd.de>
2021-04-17 18:25:35 +0200

    Update dunst config and add notification for screenshots

--- .config/i3/screenshot.sh
+++ .config/i3/screenshot.sh
@@ -8,13 +8,22 @@ case $1 in
   "window" )
     maim -i $(xdotool getactivewindow) | \
       xclip -selection clipboard -t image/png && \
-      xclip -selection clipboard -o > ${HOME}/screenshots/${FILENAME}.png;;
+      xclip -selection clipboard -o > ${HOME}/screenshots/${FILENAME}.png
+      
+    dunstify -a "screenshot" "Window Screenshot taken" "Saved as ${FILENAME}.png"
+      ;;
   "select" )
     maim -s | \
       xclip -selection clipboard -t image/png && \
-      xclip -selection clipboard -o > ${HOME}/screenshots/${FILENAME}.png;;
+      xclip -selection clipboard -o > ${HOME}/screenshots/${FILENAME}.png
+
+    dunstify -a "screenshot" "Selection Screenshot taken" "Saved as ${FILENAME}.png"
+    ;;
   "all" )
     maim | \
       xclip -selection clipboard -t image/png && \
-      xclip -selection clipboard -o > ${HOME}/screenshots/${FILENAME}.png;;
+      xclip -selection clipboard -o > ${HOME}/screenshots/${FILENAME}.png
+
+    dunstify -a "screenshot" "Full Screenshot taken" "Saved as ${FILENAME}.png"
+    ;;
 esac
