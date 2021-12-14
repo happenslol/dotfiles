492baae6e1c1ac2e6bdd0930f71972b4a0443849
Hilmar Wiegand <me@hwgnd.de>
2021-12-14 15:27:31 +0100

    Disable ssh-agent output

--- .config/zsh/ssh.zsh
+++ .config/zsh/ssh.zsh
@@ -1,17 +1,15 @@
 SSH_ENV="$HOME/.ssh/agent-environment"
 
 start_agent() {
-    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}" &> /dev/null
+    /usr/bin/ssh-agent | sed 's/^echo/#echo/' > "${SSH_ENV}"
     chmod 600 "${SSH_ENV}"
-    . "${SSH_ENV}" > /dev/null
-    /usr/bin/ssh-add > /dev/null
+    . "${SSH_ENV}" &> /dev/null
+    /usr/bin/ssh-add &> /dev/null
 }
 
-# Source SSH settings, if applicable
-
 if [ -f "${SSH_ENV}" ]; then
-  . "${SSH_ENV}" > /dev/null
-  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ > /dev/null || {
+  . "${SSH_ENV}" &> /dev/null
+  ps -ef | grep ${SSH_AGENT_PID} | grep ssh-agent$ &> /dev/null || {
       start_agent
   }
 else
