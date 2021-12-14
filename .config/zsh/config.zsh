aedf59135be332f0964fe6fd839ae177da239d36
Hilmar Wiegand <me@hwgnd.de>
2021-12-14 15:27:46 +0100

    Fix completion styling and keybinds

--- .config/zsh/config.zsh
+++ .config/zsh/config.zsh
@@ -2,16 +2,15 @@ alias svim="sudo -E nvim"
 alias reload="source ~/.zshrc"
 alias cat="bat"
 alias grep="rg"
-alias dot="/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
 
 alias ls="exa"
 alias ll="exa -la"
 alias lt="exa --tree"
 
-alias -g ...='../..'
-alias -g ....='../../..'
-alias -g .....='../../../..'
-alias -g ......='../../../../..'
+alias -g ...="cd ../.."
+alias -g ....="cd ../../.."
+alias -g .....="cd ../../../.."
+alias -g ......="cd ../../../../.."
 
 alias md='mkdir -p'
 alias rd=rmdir
@@ -30,16 +29,35 @@ setopt hist_ignore_space
 setopt hist_verify
 setopt share_history
 
+unsetopt flowcontrol
+unsetopt menu_complete
+setopt auto_menu
+setopt complete_in_word
+setopt always_to_end
+
+setopt auto_cd
+setopt multios
+setopt prompt_subst
+
+# Make sure default ls colors are set
+if [[ -z "$LS_COLORS" ]]; then
+  (( $+commands[dircolors] )) && eval "$(dircolors -b)"
+fi
+
 # Configure completion
-zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-colors "${(s.:.)LS_COLORS}"
 zstyle ':completion:*' special-dirs true
 zstyle ':completion:*' use-cache yes
 zstyle ':completion:*' cache-path $ZSH_CACHE_DIR
+zstyle ':completion:*:*:*:*:*' menu select
 
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z-_}={A-Za-z_-}' 'r:|=*' 'l:|=* r:|=*'
 zstyle ':completion:*:cd:*' tag-order local-directories directory-stack path-directories
 
 autoload -U +X bashcompinit && bashcompinit
+
+# Load required modules
+zmodload zsh/complist
 
 # Keybinds
 if (( ${+terminfo[smkx]} )) && (( ${+terminfo[rmkx]} )); then
@@ -74,3 +92,7 @@ zle -N down-line-or-beginning-search
 bindkey -M emacs "${terminfo[kcud1]}" down-line-or-beginning-search
 bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
 bindkey -M vicmd "${terminfo[kcud1]}" down-line-or-beginning-search
+
+bindkey -M menuselect '^[[Z' reverse-menu-complete
+bindkey -M menuselect '^l' accept-and-infer-next-history
+bindkey -M menuselect '^j' accept-line
